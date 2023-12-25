7bcdc8eaabbad610579ffcf4dc222b6729e8d17a
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-12-25 16:29:30 +0100

    Profile desktop screen 90% done

--- src/pages/screen/ProfileScreen.tsx
+++ src/pages/screen/ProfileScreen.tsx
@@ -10,12 +10,67 @@ import { FaRegAddressCard } from "react-icons/fa6";
 
 const ProfileScreen = () => {
   return (
-    <div className="w-full min-h-[100vh] flex justify-center">
-      <div className="max-sm:hidden w-[99%] h-[150px]  flex flex-col items-center ">
-        <div className="flex flex-col items-center mt-14">
-          <div className="w-[150px] h-[150px] rounded-full border border-black"></div>
-          <h1 className="text-2xl font-bold mt-3">Francis Uzoigwe</h1>
-          <div  className="text-[14px]">kossyuzoigwe@gmail.com</div>
+    <div className="w-full min-h-[100vh] bg-green-400 flex justify-center">
+      <div className="max-sm:hidden w-[99%] h-[150px]  flex flex-col ">
+        <div className="w-full items-center my-3 h-[200px] flex">
+          <img
+            className="w-[120px] h-[120px] ml-3 rounded-full border object-cover"
+            src=""
+            alt="Photo"
+          />
+          <div className="ml-5 text-white">
+            <div className="text-3xl font-bold font-[Ever]">
+              Kossyrisochukwu
+            </div>
+            <div className="text-sm font-bold font-[Ever]">
+              kossyuzoigwe@gmail.com
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex flex-col ">
+          <div className="w-[50%] max-lg:w-[60%]">
+            <div className="w-full my-10  ml-3 mt-3 flex flex-col text-white ">
+              <div className="flex items-center justify-between">
+                <div>
+                  <div className="font-[Ever] ">Phone:</div>
+                  <div className="px-2 border ">0704747**86</div>
+                </div>
+                <div>
+                  <div className="font-[Ever] ">Address:</div>
+                  <div className="px-2 border ">House 96B, Ago-Hausa</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full my-10  ml-3 mt-3 flex flex-col text-white">
+              <div className="flex items-center justify-between">
+                <div>
+                  <div className="font-[Ever] ">Acc. Number:</div>
+                  <div className="px-2 border ">7047474886</div>
+                </div>
+                <div className="">
+                  <div className="font-[Ever] ">Acc. Name:</div>
+                  <div className="px-2 border ">Francis Kossyrisochukwu</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full my-10  ml-3 mt-3 flex flex-col text-white">
+              <div className="flex items-center justify-between">
+                <div>
+                  <div className="font-[Ever] ">School Name:</div>
+                  <div className="px-2 border ">0704747**86</div>
+                </div>
+                <div className="">
+                  <div className="font-[Ever] ">Class:</div>
+                  <div className="px-2 border ">Francis Kossyrisochukwu</div>
+                </div>
+              </div>
+              <div className="w-full h-auto my-24 flex items-center justify-center">
+                <button className="font-[Ever] text-sm px-3 py-2 rounded-md bg-white text-green-500 hover:cursor-pointer hover:scale-105 transition-all duration-300  ">
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="max-sm:flex hidden w-[95%] h-full justify-center">
