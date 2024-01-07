a0816eafdc745ed7f9c1f063f893583a9bd4c0de
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-01-08 00:40:11 +0100

    early changes

--- src/components/static/Menue.tsx
+++ src/components/static/Menue.tsx
@@ -1,26 +1,22 @@
-
 import { useState } from "react";
 import { motion, Variants } from "framer-motion";
 import { Link } from "react-router-dom";
 import { BiMenu } from "react-icons/bi";
 
-
-
 const itemVariants: Variants = {
   open: {
     opacity: 1,
     y: 0,
-    transition: { type: "spring", stiffness: 300, damping: 24 }
+    transition: { type: "spring", stiffness: 300, damping: 24 },
   },
-  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
 };
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-
-     <motion.nav
+    <motion.nav
       initial={false}
       animate={isOpen ? "open" : "closed"}
       className="menu "
@@ -28,21 +24,25 @@ export default function App() {
       <motion.button
         whileTap={{ scale: 0.97 }}
         onClick={() => setIsOpen(!isOpen)}
-        className="w-[200px] flex  px-3 py-2 bg-transparent justify-end ">
+        className="w-[200px] flex  px-3 py-2 bg-transparent justify-end "
+      >
         {/* Menu */}
         <motion.div
           variants={{
             open: { rotate: 0 },
-            closed: { rotate: 0 }
+            closed: { rotate: 0 },
           }}
           transition={{ duration: 0.2 }}
           style={{ originY: 0.55 }}
         >
-        <div className="text-[24px]"> <BiMenu/></div>
+          <div className="text-[24px]">
+            {" "}
+            <BiMenu />
+          </div>
         </motion.div>
       </motion.button>
       <motion.ul
-         className="bg-[#e2e2e2c5] mt-3 w-[200px] px-1 py-1"
+        className="bg-[#e2e2e2c5] mt-3 w-[200px] px-1 py-1"
         variants={{
           open: {
             clipPath: "inset(0% 0% 0% 0% round 10px)",
@@ -51,7 +51,7 @@ export default function App() {
               bounce: 0,
               duration: 0.7,
               delayChildren: 0.3,
-              staggerChildren: 0.05
+              staggerChildren: 0.05,
             },
           },
           closed: {
@@ -59,21 +59,49 @@ export default function App() {
             transition: {
               type: "spring",
               bounce: 0,
-              duration: 0.3
-            }
-          }
+              duration: 0.3,
+            },
+          },
         }}
         style={{ pointerEvents: isOpen ? "auto" : "none" }}
       >
-        <Link to="/card">
-        <motion.li variants={itemVariants} className="py-2 px-1 hover:bg-white cursor-pointer rounded-sm">Card 1</motion.li>
+        <Link to="/register">
+          <motion.li
+            variants={itemVariants}
+            className="py-2 px-1 hover:bg-white cursor-pointer rounded-sm"
+          >
+            Register
+          </motion.li>
         </Link>
-        <motion.li variants={itemVariants} className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm">Item 2 </motion.li>
-        <motion.li variants={itemVariants} className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm">Item 3 </motion.li>
-        <motion.li variants={itemVariants} className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm">Item 4 </motion.li>
-        <motion.li variants={itemVariants} className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm">Item 5 </motion.li>
+        <Link to="/signin">
+          <motion.li
+            variants={itemVariants}
+            className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm"
+          >
+            Signin
+          </motion.li>
+        </Link>
+        <Link to="/">
+          <motion.li
+            variants={itemVariants}
+            className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm"
+          >
+            Home{" "}
+          </motion.li>
+        </Link>
+        <motion.li
+          variants={itemVariants}
+          className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm"
+        >
+          About{" "}
+        </motion.li>
+        <motion.li
+          variants={itemVariants}
+          className="py-2 px-1  hover:bg-white cursor-pointer rounded-sm"
+        >
+          Services{" "}
+        </motion.li>
       </motion.ul>
     </motion.nav>
-
   );
 }
